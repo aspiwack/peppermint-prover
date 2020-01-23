4d01468417d6f59656d0c2e80015b1c8554e22b7
Arnaud Spiwack <arnaud@spiwack.net>
2020-01-23 19:02:21 +0100

    doc: general thoughts and perspective about this approach to tactics

--- lensy-f/src/Control/Tactic.hs
+++ lensy-f/src/Control/Tactic.hs
@@ -2,6 +2,107 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
+
+-- | This module defines a generic type of tactics in the sense of proof
+-- assistants. This library is well-suited to give the backbone of a tactic
+-- system where you have a type of goals, proof obligation, or generally
+-- something which needs to be /solved/; and you have a type of theorem, or
+-- solution.
+--
+-- The type @'Tactic' m goal thm@ then models proof instructions of the form: if
+-- you can resolve this new collection of sub-goals, then I can build a solution
+-- for the current goal. Where goals are of type @goal@ and solutions of type
+-- @thm@. The remaining parameter @m@ represents the kind of effects that the
+-- tactics are permitted to perform (see, in particular 'proveWith'). It will
+-- typically be a 'MonadPlus', even though the tactic combinators in this module
+-- require weaker properties.
+--
+-- In practice you may want the monad @m@ to be very rich: you may want to
+-- support logging, caching, etc… But the simplest examples of choice of @m@ are
+-- 'Maybe' and '[]'. With 'Maybe', the semantics of @a `or` b@ is: if @a@
+-- succeeds then @a@ else @b@. Using '[]' adds backtracking (which can be
+-- formalised as the fact that @(a `or` b) `thn` c = (a `thn c) `or` (b `thn`
+-- c)`.)
+--
+-- == Applicability and limitations
+--
+-- The main limitation of tactics such as here is that they can only consider
+-- one goal at a time. If you need tactics which consider several goals at a
+-- time to progress, these tactics won't do. At least not easily.
+--
+-- Another case where this is not particularly well-suited is if you have a
+-- native notion of theorems-with-unproved-holes. Indeed, tactics can be seen as
+-- a way to use functions to add holes to theorems. If you already have holes,
+-- your tactics should probably be implemented in terms of these holes.
+--
+-- Both of these points are discussed, in the context of the Coq proof
+-- assistant, in Chapter 4 of
+-- <http://assert-false.net/arnaud/papers/thesis.spiwack.pdf my thesis>.
+--
+-- == Historical notes
+--
+-- Tactics were introduced in the original paper about the ML language, and are
+-- generally attributed to Robin Milner. Tactics were defined as
+--
+-- @
+-- type tactic = goal -> (goal list * (thm list -> thm))
+-- @
+--
+-- where both lists are understood to have the same length. Tactics were used to
+-- write proofs in the LCF proof assistant (ML was created to be the
+-- Meta-Language of LCF).
+--
+-- Variants of this type, generally without lists appear in the literature, with
+-- reference to Milner tactics. For example in
+-- <https://link.springer.com/chapter/10.1007/978-3-642-37036-6_2 the compiler
+-- forest> compiler passes are defined as
+--
+-- @
+-- source→(source′×(target′→target))
+-- @
+--
+-- With explicit reference to Milner's tactics.
+--
+-- A similar type also appears in Valeria de Paiva's categorical analysis of
+-- Gödel's Dialectica interpretation
+-- <https://github.com/vcvpaiva/DialecticaCategories>. I don't know if de Paiva
+-- was aware of the connection at the time, but I've seen reference to her work
+-- in comparison with Milner tactics.
+--
+-- The lenses appeared as a completely parallel and, as far as I know,
+-- independent work. I believe lens to have been introduced in the context of
+-- the <https://www.seas.upenn.edu/~harmony/ Boomerang> language. There, they
+-- were given as a pair of a getter and a setter
+--
+-- @
+-- (a -> b, a -> b -> a)
+-- @
+--
+-- In the early version of the lens library for Haskell, Edward Kmett had
+-- factored the first argument for efficiency:
+--
+-- @
+-- a -> (a, b -> a)
+-- @
+--
+-- It's obvious when everything is put on the same page, but I don't think
+-- anybody realised that these types were all the same. Not to my knowledge,
+-- anyway.
+--
+-- What's of interest though, is that the van Laarhoven presentation of lenses
+-- admits a version which is precisely the same as a lens to a list of position:
+-- traversals. What's nice about traversals is that we don't have to deal with
+-- two list which must be the same length, but can't be proved to be so by the
+-- type system. This, together with the already simplified composition of the
+-- van Laarhoven presentation compared to @a->(b,b->a)@, let me write a much
+-- simpler library.
+--
+-- There is a remaining lie which needs ironing out: Milner's tactics can
+-- fail. Traversal can have no position to fill, but this corresponds to success
+-- in Milner's tactic. So we need a little extra oomph. Which is achieve by
+-- adding an appropriate monad in the right place in the definition. While we
+-- are at it, I made the monad abstract, so the library can be multi-purpose.
+
 module Control.Tactic where
 
 import Data.Void
